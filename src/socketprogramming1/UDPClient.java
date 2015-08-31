5116ca43ca2fb37c851f607ac8a2c8e3392cc6ea
shivangi parashar <shivangi parashar@shivangi>
2015-08-31 21:14:34 +0530

    new commit

--- src/socketprogramming1/UDPClient.java
+++ src/socketprogramming1/UDPClient.java
@@ -1,5 +1,6 @@
-
+//UDPClient.java
 import java.net.DatagramPacket;
+
 import java.net.DatagramSocket;
 public class UDPClient
 {
@@ -21,6 +22,32 @@ catch(Exception e)
 {
 System.out.println("Exception :"+e);
 }
+}
+//UDPServer.java
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+public class UDPServer
+{
+public static void main(String args[])
+{
+try
+{
+Date date;
+String strDate;
+DatagramSocket ds=new DatagramSocket(4444);
+int i=0;
+while(true)
+{
+date=new Date();
+strDate=date.toString();
+byte dbyte[]=strDate.getBytes();
+InetAddress address=InetAddress.getByName("localhost");
+DatagramPacket packate=new DatagramPacket(dbyte,dbyte.length,address,5555);
+ds.send(packate);
+System.out.println((++i)+"packate sent");
+Thread.sleep(1000);
 }
 }
 
